cfb866a121fe41de7b7091e61880830f2437e746
ilievilko <ilko.i.iliev@gmail.com>
2020-03-06 23:13:47 +0200

    Initial commit

---
+++ src/main/java/entities/university/Course.java
@@ -0,0 +1,78 @@
+package entities.university;
+
+import entities.BaseEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.time.LocalDate;
+import java.util.Set;
+
+@Entity
+@Table(name = "course")
+public class Course extends BaseEntity {
+    private String name;
+    private String description;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private int credits;
+    private Set<Student> students;
+
+    public Course() {
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "description")
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Column(name = "start_date")
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    @Column(name = "end_date")
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    @Column(name = "credits")
+    public int getCredits() {
+        return credits;
+    }
+
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
+
+    @ManyToMany(mappedBy = "courses")
+    public Set<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(Set<Student> students) {
+        this.students = students;
+    }
+}
